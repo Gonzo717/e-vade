1bc5e132841e8c025ff547ce56fd4fd3e596fc71
CSSI <css@CSSs-MacBook-Air.local>
2017-07-31 20:30:16 -0500

    I linked some files and thats pretty much it

--- E-Vade(local)/main.py
+++ E-Vade(local)/main.py
@@ -18,10 +18,13 @@ import webapp2
 import jinja2
 import os
 
+jinja_environment = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        template = jinja_environment.get_template('templates/e-vade.html')
+        self.response.out.write(template.render())
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
