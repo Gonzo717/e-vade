174ab81dede81196e4923f6b58609906c69f1fa1
CSSI <css@CSSs-MacBook-Air.local>
2017-08-01 14:50:59 -0500

    Fixed model errors

--- E-Vade(local)/scoreboard.py
+++ E-Vade(local)/scoreboard.py
@@ -1,13 +1,7 @@
 from google.appengine.ext import ndb
 
-<<<<<<< HEAD
 #model for scoreboard
 
 class Scoreboard(ndb.Model)
     username = ndb.StringProperty()
     score = ndb.IntegerProperty(repeated = True) )
-=======
-class Entry(ndb.Model):
-    name = ndb.StringProperty(required = True)
-    score = ndb.IntegerProperty(repeated = True)
->>>>>>> 66bc905683bf6c468ca40adf9c91c2b024285a19
