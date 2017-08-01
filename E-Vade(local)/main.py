a10fd38ebf337d0e0d268f07d20605bcd0c918e5
CSSI <css@CSSs-MacBook-Air.local>
2017-08-01 11:18:41 -0500

    changed rankhandler

--- E-Vade(local)/main.py
+++ E-Vade(local)/main.py
@@ -34,8 +34,13 @@ class TutorialHandler(webapp2.RequestHandler):
 
 class RankHandler(webapp2.RequestHandler):
     def get(self):
-        template = jinja_environment.get_template('templates/rankings.html')
-        self.response.out.write(template.render())
+            template = jinja_environment.get_template("templates/rankings.html")
+            userName = self.request.get("name")
+            points = int(self.request.get("score"))
+            self.response.write(userName)
+            self.response.write(points)
+            my_snack = Snack(name = userName, score = points)
+            my_snack.put()
 
 class AboutHandler(webapp2.RequestHandler):
     def get(self):
