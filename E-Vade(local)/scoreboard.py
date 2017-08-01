b5e877a59a32646dad2113962b149a465acd4812
CSSI <css@CSSs-MacBook-Air.local>
2017-08-01 10:56:01 -0500

    created scoreboard base, link to database

---
+++ E-Vade(local)/scoreboard.py
@@ -0,0 +1,22 @@
+from google.appengine.ext import ndb
+import webapp2
+import os
+#import urllib2
+#import urllib
+import jinja2
+
+jinja_environment = jinja2.Environment(
+    loader = jinja2.FileSystemLoader(os.path.dirname(__file__)))
+class Entry(ndb.Model):
+    name = ndb.StringProperty(required = True)
+    score = ndb.IntegerProperty(repeated = True)
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        template = jinja_environment.get_template("templates/rankings.html")
+        userName = self.request.get("name")
+        points = int(self.request.get("score"))
+        #self.response.write(userName)
+        #self.response.write(points)
+        my_snack = Snack(name = userName, score = points)
+        my_snack.put()
