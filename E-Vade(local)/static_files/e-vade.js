444fa18039c7a4c73c08347ed84ffeca994278d0
CSSI <css@CSSs-MacBook-Air.local>
2017-08-02 17:25:30 -0500

    YAYAYYAYAYAYAYYSA!!!!!!!

--- E-Vade(local)/static_files/e-vade.js
+++ E-Vade(local)/static_files/e-vade.js
@@ -12,61 +12,81 @@ var user_location = "0_0"
 
 var myGamePiece = "#sun";
 
+
 function getX(location) {
   x_str = location.substring(0, location.indexOf('_'));
   return parseInt(x_str);
 }
 
 function getY(location) {
-  y_str = location.substring(location.indexOf('_')+1, location.length);
+  y_str = location.substring(location.indexOf('_') + 1, location.length);
   return parseInt(y_str);
 }
 
 function left() {
-  user_location = (getX(user_location)-1) + "_" + getY(user_location)//getY(parseInt(user_location+1))
-// EDGE CODE
-  if (getX(user_location)<0){
-      user_location= "0_" + getY(user_location).toString()
+  $("#" + user_location).html("");
+  user_location = (getX(user_location) - 1) + "_" + getY(user_location) //getY(parseInt(user_location+1))
+  // EDGE CODE
+  if (getX(user_location) < 0) {
+    user_location = "0_" + getY(user_location).toString()
   }
-  $("#sun").append($(user_location));
+  $("#" + user_location).html("<img src='https://pbs.twimg.com/profile_images/641353910561566720/VSxsyxs7.jpg' width='50' height='50'></img>");
+  console.log(user_location);
 }
+
 function up() {
-  user_location = getX(user_location) + "_" + (getY(user_location)-1)//getY(parseInt(user_location+1))
+  $("#" + user_location).html("");
+  user_location = getX(user_location) + "_" + (getY(user_location) - 1) //getY(parseInt(user_location+1))
+  $("#" + user_location).html("<img src='https://pbs.twimg.com/profile_images/641353910561566720/VSxsyxs7.jpg' width='50' height='50'></img>");
+
 }
+
 function right() {
-  user_location = (getX(user_location)+1) + "_" + getY(user_location)//getY(parseInt(user_location+1))
+  $("#" + user_location).html("");
+  user_location = (getX(user_location) + 1) + "_" + getY(user_location) //getY(parseInt(user_location+1))
+  $("#" + user_location).html("<img src='https://pbs.twimg.com/profile_images/641353910561566720/VSxsyxs7.jpg' width='50' height='50'></img>");
+
 }
+
 function down() {
   //return getX(user_location) + 1;
-  user_location = getX(user_location) + "_" + (getY(user_location)+1)//getY(parseInt(user_location+1))
-  $("#sun").append($(user_location));
+  $("#" + user_location).html("");
+  user_location = getX(user_location) + "_" + (getY(user_location) + 1) //getY(parseInt(user_location+1))
+  $("#" + user_location).html("<img src='https://pbs.twimg.com/profile_images/641353910561566720/VSxsyxs7.jpg' width='50' height='50'></img>");
+
 }
 $(document).bind('keypress', function(e) {
-    if(e.which === 13){
-        console.log(user_location);
-    }
+  if (e.which === 13) {
+    console.log(user_location);
+  }
 });
 $(document).keydown(function(e) {
-    if(e.which === 37){
-      //  alert("left");
-        $("#0_0").html("<img id='sun' src='https://pbs.twimg.com/profile_images/641353910561566720/VSxsyxs7.jpg' width='50' height='50'></img>");
-        console.log(user_location);
-    }
+  if (e.which === 37) {
+    //  alert("left");
+    // $("#" + user_location).html("<img src='https://pbs.twimg.com/profile_images/641353910561566720/VSxsyxs7.jpg' width='50' height='50'></img>");
+    // console.log(user_location);
+    left();
+  }
 });
 
 $(document).keydown(function(e) {
-    if(e.which === 38){
-        console.log("up");
-        console.log(user_location);
-    }
-});$(document).keydown(function(e) {
-    if(e.which === 39){
-        console.log("right");
-        console.log(user_location);
-    }
-});$(document).keydown(function(e) {
-    if(e.which === 40){
-        console.log("down");
-        console.log(user_location);
-    }
+  if (e.which === 38) {
+    console.log("up");
+    console.log(user_location);
+    up();
+  }
+});
+$(document).keydown(function(e) {
+  if (e.which === 39) {
+    console.log("right");
+    console.log(user_location);
+    right();
+  }
+});
+$(document).keydown(function(e) {
+  if (e.which === 40) {
+    console.log("down");
+    console.log(user_location);
+    down();
+  }
 });
