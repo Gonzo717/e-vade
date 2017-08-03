ff4b9aeb8936089346c5f900b7cf0d4d41d3564b
CSSI <css@CSSs-MacBook-Air.local>
2017-08-03 13:51:06 -0500

    removed those stupid merge conflict things

--- E-Vade(local)/static_files/e-vade.js
+++ E-Vade(local)/static_files/e-vade.js
@@ -1,12 +1,7 @@
 var user_location = "0_0"
 var ball = "5_0"
 
-<<<<<<< HEAD
 
-
-
-=======
->>>>>>> 55476c714464d8fc2616567426c1a90b55335d01
 function getX(location) {
   x_str = location.substring(0, location.indexOf('_'));
   return parseInt(x_str);
@@ -23,12 +18,9 @@ function ballmove() {
   $("#" + ball).html("<img src='https://opengameart.org/sites/default/files/rock_01_loop.gif' width='50' height='50'></img>");
   console.log(ball);
 }
-<<<<<<< HEAD
-=======
 setInterval(
   ballmove, 1000
 );
->>>>>>> 55476c714464d8fc2616567426c1a90b55335d01
 
 function left() {
   $("#" + user_location).html("");
@@ -72,7 +64,7 @@ $(document).bind('keypress', function(e) {
     console.log(user_location);
   }
 });
-<<<<<<< HEAD
+
 if (e.which === 13) {
   console.log(user_location);
   ballmove();
@@ -94,7 +86,6 @@ if (e.which === 13) {
 }
   $(document).ready(ballmove(ball));
   //ballmove(ball);
-=======
 $(document).keydown(function(e) {
   if (e.which === 37) {
     left();
@@ -114,27 +105,18 @@ $(document).keydown(function(e) {
   }
 });
 $(document).ready(ballmove(ball));
-<<<<<<< HEAD
-//ballmove(ball);
->>>>>>> 55476c714464d8fc2616567426c1a90b55335d01
-
-=======
->>>>>>> 98f05b74189ca08590fbcccf7c5e95fa011c522e
 
   var ar = new Array(37, 38, 39, 40)
 
-<<<<<<< HEAD
   $(document).keydown(function(e) {
     var key = e.which;
     if ($.inArray(key, ar) > -1) {
       e.preventDefault();
     }
   });
-=======
 $(document).keydown(function(e) {
   var key = e.which;
   if ($.inArray(key, ar) > -1) {
     e.preventDefault();
   }
 });
->>>>>>> 55476c714464d8fc2616567426c1a90b55335d01
