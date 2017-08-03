8cd35e17a4bc28268b5f8f11a672eec03b5982e7
CSSI <css@CSSs-MacBook-Air.local>
2017-08-02 20:33:25 -0500

    edges

--- E-Vade(local)/static_files/e-vade.js
+++ E-Vade(local)/static_files/e-vade.js
@@ -1,28 +1,13 @@
 var user_location = "0_0"
 
-//var character = 800
-
-//user_location = character
-// $(document).bind('keypress', function(e) {
-//     if(e.which === 13){
-//         console.log(user_location);
-//         $("#sun").append($(user_location));
-//     }
-// });
-
-var myGamePiece = "#sun";
-
-
 function getX(location) {
   x_str = location.substring(0, location.indexOf('_'));
   return parseInt(x_str);
 }
-
 function getY(location) {
   y_str = location.substring(location.indexOf('_') + 1, location.length);
   return parseInt(y_str);
 }
-
 function left() {
   $("#" + user_location).html("");
   user_location = (getX(user_location) - 1) + "_" + getY(user_location) //getY(parseInt(user_location+1))
@@ -33,34 +18,32 @@ function left() {
   $("#" + user_location).html("<img src='https://pbs.twimg.com/profile_images/641353910561566720/VSxsyxs7.jpg' width='50' height='50'></img>");
   console.log(user_location);
 }
-
 function up() {
   $("#" + user_location).html("");
   user_location = getX(user_location) + "_" + (getY(user_location) - 1) //getY(parseInt(user_location+1))
-  $("#" + user_location).html("<img src='https://pbs.twimg.com/profile_images/641353910561566720/VSxsyxs7.jpg' width='50' height='50'></img>");
   if (getY(user_location) < 0) {
     user_location = getX(user_location).toString() + "_0"
   }
+  $("#" + user_location).html("<img src='https://pbs.twimg.com/profile_images/641353910561566720/VSxsyxs7.jpg' width='50' height='50'></img>");
 }
-
 function right() {
   $("#" + user_location).html("");
   user_location = (getX(user_location) + 1) + "_" + getY(user_location) //getY(parseInt(user_location+1))
-  $("#" + user_location).html("<img src='https://pbs.twimg.com/profile_images/641353910561566720/VSxsyxs7.jpg' width='50' height='50'></img>");
   if (getX(user_location) > 9) {
     user_location = "9_" + getY(user_location).toString()
   }
+  $("#" + user_location).html("<img src='https://pbs.twimg.com/profile_images/641353910561566720/VSxsyxs7.jpg' width='50' height='50'></img>");
 }
-
 function down() {
   //return getX(user_location) + 1;
   $("#" + user_location).html("");
   user_location = getX(user_location) + "_" + (getY(user_location) + 1) //getY(parseInt(user_location+1))
-  $("#" + user_location).html("<img src='https://pbs.twimg.com/profile_images/641353910561566720/VSxsyxs7.jpg' width='50' height='50'></img>");
   if (getY(user_location) > 9) {
     user_location = getX(user_location).toString() + "_9"
   }
+  $("#" + user_location).html("<img src='https://pbs.twimg.com/profile_images/641353910561566720/VSxsyxs7.jpg' width='50' height='50'></img>");
 }
+
 $(document).bind('keypress', function(e) {
   if (e.which === 13) {
     console.log(user_location);
@@ -68,31 +51,33 @@ $(document).bind('keypress', function(e) {
 });
 $(document).keydown(function(e) {
   if (e.which === 37) {
-    //  alert("left");
-    // $("#" + user_location).html("<img src='https://pbs.twimg.com/profile_images/641353910561566720/VSxsyxs7.jpg' width='50' height='50'></img>");
-    // console.log(user_location);
     left();
+  }
+  if (e.which === 38) {
+    up();
+  }
+  if (e.which === 39) {
+    right();
+  }
+  if (e.which === 40) {
+    down();
   }
 });
 
+  // $(document).keydown(function(e) {
+  //   if (e.which === 40) {
+  //     // console.log("down");
+  //     // console.log(user_location);
+  //     down();
+  //   }
+  // });
+var ar = new Array(37, 38, 39, 40)
+
 $(document).keydown(function(e) {
-  if (e.which === 38) {
-    console.log("up");
-    console.log(user_location);
-    up();
-  }
-});
-$(document).keydown(function(e) {
-  if (e.which === 39) {
-    console.log("right");
-    console.log(user_location);
-    right();
-  }
-});
-$(document).keydown(function(e) {
-  if (e.which === 40) {
-    console.log("down");
-    console.log(user_location);
-    down();
-  }
+  var key = e.which;
+  if ($.inArray(key, ar) > -1) {
+  e.preventDefault();
+  return false;
+}
+return false;
 });
