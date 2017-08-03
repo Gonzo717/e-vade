a066ea9ab20d0b6a84f27af6c97b5eac566d40cc
CSSI <css@CSSs-MacBook-Air.local>
2017-08-03 11:04:21 -0500

    enter button

--- E-Vade(local)/static_files/e-vade.js
+++ E-Vade(local)/static_files/e-vade.js
@@ -1,11 +1,9 @@
 var user_location = "0_0"
-var ball = "#5_0"
+var ball = "5_0"
 
-function ballmove() {
-$(ball).html("<img src='https://opengameart.org/sites/default/files/rock_01_loop.gif' width='50' height='50'></img>");
-}
 
-ballmove()
+
+
 function getX(location) {
   x_str = location.substring(0, location.indexOf('_'));
   return parseInt(x_str);
@@ -14,6 +12,13 @@ function getX(location) {
 function getY(location) {
   y_str = location.substring(location.indexOf('_') + 1, location.length);
   return parseInt(y_str);
+}
+function ballmove() {
+  // $("#" + ball).html("<img src='https://pbs.twimg.com/profile_images/641353910561566720/VSxsyxs7.jpg' width='50' height='50'></img>");
+  ball = getX(ball) + "_" + getY(ball); //getY(parseInt(user_location+1))
+  // EDGE CODE
+  $("#" + ball).html("<img src='http://bestanimations.com/Earth&Space/astronaut-animation-9.gif' width='50' height='50'></img>");
+  console.log(ball);
 }
 function left() {
   $("#" + user_location).html("");
@@ -69,7 +74,13 @@ $(document).keydown(function(e) {
   if (e.which === 40) {
     down();
   }
+  if (e.which === 13) {
+    console.log(user_location);
+    ballmove();
+  }
 });
+$( document ).ready(ballmove(ball));
+//ballmove(ball);
 
   // $(document).keydown(function(e) {
   //   if (e.which === 40) {
